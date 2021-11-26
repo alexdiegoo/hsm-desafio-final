57b8d7b6bef4e2cd328a50180d7481430596f153
Alex Diego <alex.diegosn10@gmail.com>
2021-11-26 00:27:41 -0300

    refactor: :recycle: opção para update

--- saveToDatabase.js
+++ saveToDatabase.js
@@ -1,15 +1,19 @@
 const fs = require('fs');
 
-function saveToDatabase(course) {
-  try {
-    const data = fs.readFileSync('./database.json');
-    const db = JSON.parse(data);
-    db.courses.push(course);
+function saveToDatabase(course, update = false) {
+  const db = JSON.parse(fs.readFileSync('./database.json'));
 
+  if(update) {
+    const updateCourses = db.courses.map(courseArray => courseArray.id === course.id ? course : courseArray);
+    db.courses = updateCourses;
+  }
+  else db.courses.push(course);
+
+  try {
     const json = JSON.stringify(db, null, 2);
     fs.writeFileSync('./database.json', json);
   } catch(err) {
-    console.log("Houve um erro ao criar o curso. " + err);
+    console.log("Houve um erro ao gravar os dados. " + err);
   }
 }
 
